4d2b4b77b6638fb329c8c195815ef07505c6b615
andreaBuenav <andreabg1393@gmail.com>
2024-08-28 15:07:16 -0500

    Services made

---
+++ src/main/java/com/globant/services/UserService.java
@@ -0,0 +1,32 @@
+package com.globant.services;
+
+import com.globant.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserService {
+    private final static Map<String , User> users = new HashMap<>();
+
+    //Sign up parameters
+    //Adding users
+    public void addUser(User user){
+        users.put(user.getMail(), user);
+    }
+    //Checks if mail is already in use
+    public boolean isRegistered(String mail){
+        return users.containsKey(mail);
+    }
+
+    //Login parameters
+    //find by mail
+    public User findUser(String mail){
+        isRegistered(mail);
+        return users.get(mail);
+    }
+    //Validating mail and password
+    public boolean userValidator(String mail, String password){
+        User user = findUser(mail);
+        return user != null && user.getPassword().equals(password);
+    }
+}
