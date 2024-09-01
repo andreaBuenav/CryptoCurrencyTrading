1c4d68a35e3a720f8f97af0db954165ac93bdbf7
andreaBuenav <andreabg1393@gmail.com>
2024-08-31 21:11:01 -0500

    Final Update!!!!!!

---
+++ src/main/java/com/globant/model/Orders/PurchaseOrder.java
@@ -0,0 +1,32 @@
+package com.globant.model.Orders;
+
+import com.globant.model.CryptoCurrencies.CryptoCurrency;
+import com.globant.model.User;
+import com.globant.model.Wallet;
+import com.globant.model.exceptions.InsufficientCryptoException;
+import com.globant.model.exceptions.InsufficientFundsException;
+
+import java.math.BigDecimal;
+
+public class PurchaseOrder extends Order {
+    private BigDecimal amount;
+    private BigDecimal price;
+
+    public PurchaseOrder(User user, CryptoCurrency crypto, BigDecimal amount, BigDecimal price) {
+            super(user, crypto);
+            this.amount = amount;
+            this.price = price;
+        }
+
+        public BigDecimal getAmount() {
+            return amount;
+        }
+
+        public void setAmount(BigDecimal amount) {
+            this.amount = amount;
+        }
+
+        public BigDecimal getPrice() {
+            return price;
+        }
+}
